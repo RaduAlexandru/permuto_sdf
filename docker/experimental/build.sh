61b4b32b0d27692559fa5ff1f59a47bd96c6b838
Radu Alexandru Rosu <alejandro.rosu@gmail.com>
2023-04-05 16:42:34 +0000

    Added experimental docker for torch 2.0

---
+++ docker/experimental/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Check args
+if [ "$#" -ne 0 ]; then
+  echo "usage: ./build.sh"
+  return 1
+fi
+
+source config.txt
+
+# Build the docker image
+docker build\
+  --build-arg user=$USER\
+  --build-arg uid=$UID\
+  -t permuto_sdf_img \
+  -f Dockerfile .
+
