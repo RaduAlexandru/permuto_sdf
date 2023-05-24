73f59aafacaf75bd4d34ecba232ff2f6601551e3
Radu Alexandru Rosu <alejandro.rosu@gmail.com>
2023-05-24 18:14:09 +0000

    Render orbit also can render arcs

--- permuto_sdf_py/experiments/visualization/render_orbit.py
+++ permuto_sdf_py/experiments/visualization/render_orbit.py
@@ -4,7 +4,7 @@
 
 ###CALL with 
 #where ckpt_path is the checkpoints path and the  out_path is where you want to store your images
-# ./permuto_sdf_py/experiments/visualization/render_orbit.py --ckpt_path <ckpt_path> [ --out_path <out_path>  --no_viewer] 
+# ./permuto_sdf_py/experiments/visualization/render_orbit.py --ckpt_path <ckpt_path> --movement_type orbit [ --out_path <out_path>  --no_viewer] 
 
 
 
@@ -38,6 +38,7 @@ from permuto_sdf_py.utils.common_utils import create_bb_for_dataset
 from permuto_sdf_py.utils.common_utils import nchw2lin
 from permuto_sdf_py.utils.common_utils import lin2nchw
 from permuto_sdf_py.utils.common_utils import rotate_normals_to_cam_frame
+from permuto_sdf_py.utils.common_utils import linear2color_corr
 from permuto_sdf_py.utils.permuto_sdf_utils import load_from_checkpoint
 from permuto_sdf_py.train_permuto_sdf import run_net_in_chunks
 from permuto_sdf_py.train_permuto_sdf import HyperParamsPermutoSDF
@@ -56,6 +57,46 @@ torch.set_grad_enabled(False)
 config_path=os.path.join( os.path.dirname( os.path.realpath(__file__) ) , '../../../config', config_file)
 
 
+class CamMovement:
+    def __init__(self, movement_type):
+        self.movement_type=movement_type
+    
+    def init_cam(self):
+        if self.movement_type=="orbit":
+            cam=Camera()
+            cam.set_position([0,0,1])
+            cam.set_lookat([0,0,0])
+            cam.push_away_by_dist(0.8)
+            cam.orbit_x(-30)
+        elif self.movement_type=="arc":
+            cam_init=" 0.480362 0.0786047   1.08668 -0.0146451   0.193463 0.00288809 0.980992 0.0419963 0.0441229 0.0185097 35.357 0.0502494 5024.94" #multidace 7
+            cam=Camera()
+            cam.from_string(cam_init)
+            cam.m_exposure=1.0
+            self.x_axis=cam.cam_axes()[:,0]
+            self.y_axis=cam.cam_axes()[:,1]
+            self.radius_arc=0.3
+            self.steps_arc=180
+            self.cam_init_origin=cam.position()
+
+        return cam
+
+    def move_cam(self, cam, idx):
+        if self.movement_type=="orbit":
+            cam.orbit_y(1)
+        elif self.movement_type=="arc":
+            idx_01=float(idx)/self.steps_arc
+            #move the camra in x and y plane to describe a circle 
+            mov_x=self.x_axis*self.radius_arc*math.cos(idx_01*2*math.pi)
+            mov_y=self.y_axis*self.radius_arc*math.sin(idx_01*2*math.pi)
+            new_position=self.cam_init_origin+mov_x+mov_y
+            cam.set_position(new_position)
+
+        
+
+
+
+
 
 
 
@@ -64,6 +105,7 @@ def run():
     parser = argparse.ArgumentParser(description='prepare dtu evaluation')
     parser.add_argument('--ckpt_path', required=True,  default="",  help="checkpoint_path")
     parser.add_argument('--out_path', default="",  help="out_path")
+    parser.add_argument('--movement_type', default="orbit",  help="movement_type that the camera should do, which can be either oribt or arc")
     parser.add_argument('--no_viewer', action='store_true', help="Set this to true in order disable the viewer")
     parser.add_argument('--with_mask', action='store_true', help="Set this to true in order to train with a mask")
     args = parser.parse_args()
@@ -108,11 +150,8 @@ def run():
 
     
 
-    cam=Camera()
-    cam.set_position([0,0,1])
-    cam.set_lookat([0,0,0])
-    cam.push_away_by_dist(0.8)
-    cam.orbit_x(-30)
+    cam_mover=CamMovement(args.movement_type)
+    cam=cam_mover.init_cam()
 
     idx=0
     while True:
@@ -129,12 +168,13 @@ def run():
         # vis_height=100
         frame_to_render=Frame()
         frame_to_render.from_camera(cam, vis_width, vis_height)
-        cam.orbit_y(1)
+        cam_mover.move_cam(cam, idx)
 
         
 
         # ######RENDER##############################
         pred_rgb_img, pred_rgb_bg_img, pred_normals_img, pred_weights_sum_img=run_net_in_chunks(frame_to_render, chunk_size, args, hyperparams, model_sdf, model_rgb, model_bg, occupancy_grid, iter_nr_for_anneal, cos_anneal_ratio, hyperparams.forced_variance_finish) 
+        # pred_rgb_img=linear2color_corr(pred_rgb_img.cpu(), dim=1).clamp(0, 1).float().cuda() ##optioanl
         #view rgb
         pred_rgb_mat=tensor2mat(pred_rgb_img.detach()).rgb2bgr().to_cv8u()
         pred_rgba_img=torch.cat([pred_rgb_img,pred_weights_sum_img],1)
@@ -170,6 +210,9 @@ def run():
             pred_normals_img_cam_coords_vis_mat.to_file( os.path.join(args.out_path,"normal",str(idx)+".png") )
 
         idx+=1
+
+        if idx==360:
+            break
             
 
 
